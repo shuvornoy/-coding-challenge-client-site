4961a6c71ccd8f3880fcc4cbcc6ec9b5929f6898
shuvornoy <shuvornoy@gmaol.com>
2023-02-14 11:48:22 +0600

    navbar and footer add

--- src/Component/Router/Router.js
+++ src/Component/Router/Router.js
@@ -1,17 +1,25 @@
 import EditeData from "../EditeData/EditeData";
 import Home from "../Home/Home";
+import Main from "../Layout/Main";
+
 
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/update/:id",
-    element: <EditeData />,
-    // loader: ({ params }) => fetch(`https://coding-challenge-server-lovat.vercel.app/update/${params.id}`),
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/update/:id",
+        element: <EditeData />,
+        // loader: ({ params }) => fetch(`https://coding-challenge-server-lovat.vercel.app/update/${params.id}`),
+      }
+    ],
   },
 ]);
 export default router;
